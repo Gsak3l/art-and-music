5d5fcb4432030f8496490ba29f32e1ad0d805618
gsak3l <gsakel5@gmail.com>
2022-07-29 19:43:50 +0300

    New musical instruments v-for added. Added v-for to replace the html code of the new musical instruments with a loop. I had to create an array with the fields of title, subtitle, price, and image src for each image

--- src/components/home/AppNew.vue
+++ src/components/home/AppNew.vue
@@ -1,115 +1,114 @@
 <template>
-	<div id="new-musical-instruments"></div>
-	<section id="portfolio" class="bg-light">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 text-center">
-					<h2 class="text-uppercase section-heading">Καινουργια πνευστα μουσικα οργανα</h2>
-					<div class="section-subheading text-muted">
-						<br />
-						Κλαρίνα Buffet Crampon, Selmer, Keilwerth, Orsi, Leblanc.
-						<br />
-						Φλάουτα Buffet, Selmer, Trevor James.
-						<br />
-						Σαξόφωνα (σοπράνο, άλτο, τενόρο), Yanagisawa, Selmer, Bundy, Jupiter, Trevor James.
-						<br />
-						Τρομπέτες Bach, Conn, King, Schilke, Besson, Holton, B&amp;H.
-						<br />
-						Τρομπόνα Bach, Besson, B&amp;H.
-						<br />
-						Φλικόρνα Bach. Κόρνα (μονό, διπλό), Holton. Βαρύτονα Besson, Jupiter, B&amp;H.
-						<br />
-						Ευφώνια Besson, Jupiter, B&amp;H, Wilson.
-						<br />
-						Τούμπα (μι ύφεση, σι ύφεση), Besson, B&amp;H, Amati, Wilson.
-						<br />
-						Φαγκότα (κατά παραγγελία).&nbsp;
-					</div>
-					<br />
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid" src="assets/img/new%20musical%20instruments/bessonBE800.jpg" />
-					<div class="portfolio-caption">
-						<h4>Besson BE800</h4>
-						<p class="text-muted"><strong>Τρομπέτα Besson BE800</strong></p>
-						<p class="text-muted"><strong>700€</strong></p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid" src="assets/img/new%20musical%20instruments/BachStradivarius183G.jpg" />
-					<div class="portfolio-caption">
-						<h4>Bach Stradivarius 183G</h4>
-						<p class="text-muted"><strong>Φλικόρνο Bach Stradivarius 183G</strong></p>
-						<p class="text-muted"><strong>2000€</strong></p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid" src="assets/img/new%20musical%20instruments/BachStradivariusC180L.jpg" />
-					<div class="portfolio-caption">
-						<h4>Stradivarius C180L</h4>
-						<p class="text-muted"><strong>Τρομπέτα Bach C180L</strong></p>
-						<p class="text-muted"><strong>2000€</strong></p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid" src="assets/img/new%20musical%20instruments/bbBuffetB10.jpg" />
-					<div class="portfolio-caption">
-						<h4>Bb Buffet B10</h4>
-						<p class="text-muted"><strong>Κλαρίνο Bb Buffet B10</strong></p>
-						<p class="text-muted"><strong>500€</strong></p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid img-fluid" src="assets/img/new%20musical%20instruments/bbBuffetRC.jpg" />
-					<div class="portfolio-caption">
-						<h4>Bb Buffet RC</h4>
-						<p class="text-muted"><strong>Κλαρίνο Bb Buffet RC</strong></p>
-						<p class="text-muted"><strong>2000€</strong></p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid" src="assets/img/new%20musical%20instruments/YanagisawaA990-01.jpg" />
-					<div class="portfolio-caption">
-						<h4>Yanagisawa A990</h4>
-						<p class="text-muted"><strong>Σαξόφωνο Yanagisawa A990</strong></p>
-						<p class="text-muted"><strong>2500€</strong></p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid" src="assets/img/used%20musical%20instruments/jupiterJAS565.jpg" />
-					<div class="portfolio-caption">
-						<h4>Jupiter JAS-565</h4>
-						<p class="text-muted"><strong>Σαξόφωνο Jupiter JAS-565</strong></p>
-						<p class="text-muted"><strong>600€</strong></p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid" src="assets/img/new%20musical%20instruments/selmerFL300.jpg" />
-					<div class="portfolio-caption">
-						<h4>Selmer FL300</h4>
-						<p class="text-muted"><strong>Φλάουτο Selmer FL300</strong></p>
-						<p class="text-muted"><strong>450€</strong></p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 portfolio-item">
-					<img class="img-fluid" src="assets/img/new%20musical%20instruments/bachTR300.jpg" />
-					<div class="portfolio-caption">
-						<h4>Bach TR300</h4>
-						<p class="text-muted"><strong>Τρομπέτα Bach TR300</strong></p>
-						<p class="text-muted"><strong>600€</strong></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+  <div id="new-musical-instruments"></div>
+  <section id="portfolio" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="text-uppercase section-heading">Καινουργια πνευστα μουσικα οργανα</h2>
+          <div class="section-subheading text-muted">
+            <br/>
+            Κλαρίνα Buffet Crampon, Selmer, Keilwerth, Orsi, Leblanc.
+            <br/>
+            Φλάουτα Buffet, Selmer, Trevor James.
+            <br/>
+            Σαξόφωνα (σοπράνο, άλτο, τενόρο), Yanagisawa, Selmer, Bundy, Jupiter, Trevor James.
+            <br/>
+            Τρομπέτες Bach, Conn, King, Schilke, Besson, Holton, B&amp;H.
+            <br/>
+            Τρομπόνα Bach, Besson, B&amp;H.
+            <br/>
+            Φλικόρνα Bach. Κόρνα (μονό, διπλό), Holton. Βαρύτονα Besson, Jupiter, B&amp;H.
+            <br/>
+            Ευφώνια Besson, Jupiter, B&amp;H, Wilson.
+            <br/>
+            Τούμπα (μι ύφεση, σι ύφεση), Besson, B&amp;H, Amati, Wilson.
+            <br/>
+            Φαγκότα (κατά παραγγελία).&nbsp;
+          </div>
+          <br/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-4 portfolio-item" v-for="instrument in new_musical_instruments"
+             :key="instrument.id">
+          <img class="img-fluid" v-bind:src="instrument.src" alt=""/>
+          <div class="portfolio-caption">
+            <h4>{{ instrument.title }}</h4>
+            <p class="text-muted"><strong>{{ instrument.subtitle }}</strong></p>
+            <p class="text-muted"><strong>{{ instrument.price }}</strong></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-	export default {
-		name: "AppNew",
-	};
+export default {
+  name: "AppNew",
+
+  data() {
+    return {
+      new_musical_instruments: [
+        {
+          title: 'Besson BE800',
+          subtitle: 'Τρομπέτα Besson BE800',
+          price: '700€',
+          src: 'assets/img/new%20musical%20instruments/bessonBE800.jpg'
+        },
+        {
+          title: 'Bach Stradivarius 183G',
+          subtitle: 'Φλικόρνο Bach Stradivarius 183G',
+          price: '2000€',
+          src: 'assets/img/new%20musical%20instruments/BachStradivarius183G.jpg'
+        },
+        {
+          title: 'Stradivarius C180L',
+          subtitle: 'Τρομπέτα Bach C180L',
+          price: '2000€',
+          src: 'assets/img/new%20musical%20instruments/BachStradivariusC180L.jpg'
+        },
+        {
+          title: 'Bb Buffet B10',
+          subtitle: 'Τρομπέτα Besson BE800',
+          price: '500€',
+          src: 'assets/img/new%20musical%20instruments/bbBuffetB10.jpg'
+        },
+        {
+          title: 'Bb Buffet RC',
+          subtitle: 'Κλαρίνο Bb Buffet RC',
+          price: '2000€',
+          src: 'assets/img/new%20musical%20instruments/bbBuffetRC.jpg'
+        },
+        {
+          title: 'Yanagisawa A990',
+          subtitle: 'Σαξόφωνο Yanagisawa A990',
+          price: '2500€',
+          src: 'assets/img/new%20musical%20instruments/YanagisawaA990-01.jpg'
+        },
+        {
+          title: 'Jupiter JAS-565',
+          subtitle: 'Σαξόφωνο Jupiter JAS-565',
+          price: '600€',
+          src: 'assets/img/used%20musical%20instruments/jupiterJAS565.jpg'
+        },
+        {
+          title: 'Selmer FL300',
+          subtitle: 'Φλάουτο Selmer FL300',
+          price: '450€',
+          src: "assets/img/new%20musical%20instruments/selmerFL300.jpg"
+        },
+        {
+          title: 'Bach TR300',
+          subtitle: 'Τρομπέτα Bach TR300',
+          price: '600€',
+          src: "assets/img/new%20musical%20instruments/bachTR300.jpg"
+        },
+      ]
+    }
+  }
+
+};
 </script>
 
 <style scoped></style>
