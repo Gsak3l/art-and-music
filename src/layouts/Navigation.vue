22f40a16764780ee3ec8f5679c76a4fda5fb12b4
gsak3l <gsakel5@gmail.com>
2022-08-09 19:29:48 +0300

    navigation menu replaced multiple lines of code with v-for

--- src/layouts/Navigation.vue
+++ src/layouts/Navigation.vue
@@ -1,19 +1,19 @@
 <template>
   <nav
-    class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark"
-    id="mainNav"
+      class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark"
+      id="mainNav"
   >
     <div class="container">
       <a class="navbar-brand" href="/">Art and Music</a>
       <button
-        data-toggle="collapse"
-        data-target="#navbarResponsive"
-        class="navbar-toggler navbar-toggler-right"
-        type="button"
-        data-toogle="collapse"
-        aria-controls="navbarResponsive"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          class="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toogle="collapse"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
       >
         <i class="fa fa-bars"></i>
       </button>
@@ -22,26 +22,26 @@
           <div v-for="item in menu_items" :key="item.id">
             <li class="nav-item" role="presentation" v-if="!item.drop">
               <a class="nav-link js-scroll-trigger" :href="item.link">{{
-                item.text
-              }}</a>
+                  item.text
+                }}</a>
             </li>
             <li class="nav-item dropdown" v-else>
               <a
-                class="dropdown-toggle nav-link"
-                data-toggle="dropdown"
-                aria-expanded="false"
-                :href="item.link"
-                >{{ item.text }}</a
+                  class="dropdown-toggle nav-link"
+                  data-toggle="dropdown"
+                  aria-expanded="false"
+                  :href="item.link"
+              >{{ item.text }}</a
               >
               <div class="dropdown-menu" role="menu">
                 <a
-                  v-for="drop_item in item.drop_items"
-                  :key="drop_item.id"
-                  class="dropdown-item"
-                  role="presentation"
-                  :href="drop_item.link"
-                  style="font-size: 13px; font-family: Montserrat, sans-serif"
-                  >{{ drop_item.text }}</a
+                    v-for="drop_item in item.drop_items"
+                    :key="drop_item.id"
+                    class="dropdown-item"
+                    role="presentation"
+                    :href="drop_item.link"
+                    style="font-size: 13px; font-family: Montserrat, sans-serif"
+                >{{ drop_item.text }}</a
                 >
               </div>
             </li>
@@ -59,7 +59,7 @@ export default {
   data() {
     return {
       menu_items: [
-        { text: "ποιοι ειμαστε", link: "/#about", drop: false },
+        {text: "ποιοι ειμαστε", link: "/#about", drop: false},
         {
           text: "υπηρεσιεσ",
           link: "/#services",
@@ -104,12 +104,12 @@ export default {
           link: "#",
           drop: true,
           drop_items: [
-            { text: "εξαρτηματα", link: "/#spare-parts", drop: false },
-            { text: "αναλωσιμα", link: "/#consumables", drop: false },
+            {text: "εξαρτηματα", link: "/#spare-parts", drop: false},
+            {text: "αναλωσιμα", link: "/#consumables", drop: false},
           ],
         },
-        { text: "οδηγιεσ", link: "/tutorials", drop: false },
-        { text: "επικοινωνια", link: "/#contact", drop: false },
+        {text: "οδηγιεσ", link: "/tutorials", drop: false},
+        {text: "επικοινωνια", link: "/#contact", drop: false},
       ],
     };
   },
