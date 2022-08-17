638dcb7800a395fea3bf84aa9f088f38cd1fb34f
gsak3l <gsakel5@gmail.com>
2022-08-17 19:24:11 +0300

    Trying to make netlify web crawlers detect my contact form, vol17

--- src/components/home/AppContactTest.vue
+++ src/components/home/AppContactTest.vue
@@ -5,8 +5,8 @@
   <form name="contactForm" action="/success" method="post">
     <input type="hidden" name="contact" value="contactForm"/>
 
-    <input name="Όνομα" type="name" placeholder="Όνομα *">
-    <input name="Επώνυμο" type="surname" placeholder="Επώνυμο *">
+<!--    <input name="Όνομα" type="name" placeholder="Όνομα *">-->
+<!--    <input name="Επώνυμο" type="surname" placeholder="Επώνυμο *">-->
     <input type="Email" name="email" placeholder="Email *">
     <button id="sendMessageButton" type="submit">αποστολη</button>
 
