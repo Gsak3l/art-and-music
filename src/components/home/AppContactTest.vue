8b24914cdf4143844c7827176a22adb51bcf2ef1
gsak3l <gsakel5@gmail.com>
2022-08-17 19:11:13 +0300

    Trying to make netlify web crawlers detect my contact form, vol15

--- src/components/home/AppContactTest.vue
+++ src/components/home/AppContactTest.vue
@@ -1,5 +1,7 @@
 <template>
 
+
+
   <form name="MYFORM" action="/success" method="post">
     <input type="hidden" name="form-name" value="MYFORM"/>
     <input type="email" name="email" placeholder="email address">
